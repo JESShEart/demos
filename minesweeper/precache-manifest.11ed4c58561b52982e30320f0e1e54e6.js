92e182267c4e845c4dd4dea4610b6cef0f5f50ea
JESShEart <jeshrt@protonmail.com>
2020-04-25 16:52:20 -0400

    added minesweeper demo

---
+++ minesweeper/precache-manifest.11ed4c58561b52982e30320f0e1e54e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc50fce3e8321f8689046d4c2c0b212e",
+    "url": "/assets/icons/mine.png"
+  },
+  {
+    "revision": "6b6a84d2f5cfcbf4e9d0",
+    "url": "/bundle.6b6a8.js"
+  },
+  {
+    "revision": "6b6a84d2f5cfcbf4e9d0",
+    "url": "/bundle.c172c.css"
+  },
+  {
+    "revision": "2318557572d7698b10328206a4e67e6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ea856636118a96855c",
+    "url": "/polyfills.85ea8.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "48b01e1cb6944c0261845296bdaa6c6f",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
