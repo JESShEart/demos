a639b80c73f2f7d4bde7779d775ee9528b049bc5
JESShEart <jeshrt@protonmail.com>
2020-04-25 19:49:29 -0400

    added minesweeper demo

---
+++ minesweeper/precache-manifest.6b44388b3d7781a026e17a34182acd38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc50fce3e8321f8689046d4c2c0b212e",
+    "url": "/demos/minesweeper/assets/icons/mine.png"
+  },
+  {
+    "revision": "1b8c11a47ae18867de86",
+    "url": "/demos/minesweeper/bundle.1b8c1.js"
+  },
+  {
+    "revision": "1b8c11a47ae18867de86",
+    "url": "/demos/minesweeper/bundle.fc333.css"
+  },
+  {
+    "revision": "b07a74d83af03582636d4e8311b5c108",
+    "url": "/demos/minesweeper/index.html"
+  },
+  {
+    "revision": "0f54b9b0c0a109eccaa9",
+    "url": "/demos/minesweeper/polyfills.0f54b.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/demos/minesweeper/sw-debug.js"
+  },
+  {
+    "revision": "dd31ca287185129138f674b0c39a0c7e",
+    "url": "/demos/minesweeper/sw-esm.js"
+  },
+  {
+    "revision": "73cda4216e2be020687dba15a6b31e63",
+    "url": "/demos/minesweeper/sw.js"
+  }
+]);
