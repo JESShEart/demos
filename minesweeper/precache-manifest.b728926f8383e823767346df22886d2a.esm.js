4e74f38deacf1175af1d8bf1a26cec7885cb10fa
JESShEart <jeshrt@protonmail.com>
2020-05-09 23:18:52 -0400

    fixed the routing - built from the release branch this time

---
+++ minesweeper/precache-manifest.b728926f8383e823767346df22886d2a.esm.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc50fce3e8321f8689046d4c2c0b212e",
+    "url": "/demos/minesweeper/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "bc50fce3e8321f8689046d4c2c0b212e",
+    "url": "/demos/minesweeper/assets/icons/mine.png"
+  },
+  {
+    "revision": "7e385a9ccc9df5d551c7",
+    "url": "/demos/minesweeper/bundle.1598b.esm.js"
+  },
+  {
+    "revision": "7e385a9ccc9df5d551c7",
+    "url": "/demos/minesweeper/bundle.fc333.css"
+  },
+  {
+    "revision": "34652d989eee0a12033a9009e5735775",
+    "url": "/demos/minesweeper/index.html"
+  },
+  {
+    "revision": "0f54b9b0c0a109eccaa9",
+    "url": "/demos/minesweeper/polyfills.d948b.esm.js"
+  }
+]);
---
+++ minesweeper/precache-manifest.c1c234c3b4d8f846863c398a6b4532bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc50fce3e8321f8689046d4c2c0b212e",
+    "url": "/demos/minesweeper/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "bc50fce3e8321f8689046d4c2c0b212e",
+    "url": "/demos/minesweeper/assets/icons/mine.png"
+  },
+  {
+    "revision": "7e385a9ccc9df5d551c7",
+    "url": "/demos/minesweeper/bundle.7e385.js"
+  },
+  {
+    "revision": "7e385a9ccc9df5d551c7",
+    "url": "/demos/minesweeper/bundle.fc333.css"
+  },
+  {
+    "revision": "34652d989eee0a12033a9009e5735775",
+    "url": "/demos/minesweeper/index.html"
+  },
+  {
+    "revision": "0f54b9b0c0a109eccaa9",
+    "url": "/demos/minesweeper/polyfills.0f54b.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/demos/minesweeper/sw-debug.js"
+  },
+  {
+    "revision": "749b9ff89d08e829088e871996b2cb49",
+    "url": "/demos/minesweeper/sw-esm.js"
+  },
+  {
+    "revision": "73cda4216e2be020687dba15a6b31e63",
+    "url": "/demos/minesweeper/sw.js"
+  }
+]);
